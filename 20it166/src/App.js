e2e34cb461ecd1fa238f8d3ea8d63053969e395a
karan chauhan <88426014+karanchauhan281002@users.noreply.github.com>
2022-08-22 11:33:04 +0530

    Add files via upload

---
+++ 20it166/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddPatient from "./patient/AddPatient";
+import EditPatient from "./patient/EditPatient";
+// import ViewPatient from "./users/ViewPatient";
+
+function App() {
+  return (
+      <div className="App">
+        <Router>
+          <Navbar />
+
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/addpatient" element={<AddPatient />} />
+            <Route exact path="/editpatient/:id" element={<EditPatient />} />
+            {/* <Route exact path="/editpatient/:id" element={<ViewPatient />} /> */}
+          </Routes>
+        </Router>
+      </div>
+  );
+}
+
+export default App;
